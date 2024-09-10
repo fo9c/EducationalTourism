ef6750bc4d861805e7b932697330c2378736db2e
fo9c <1974555620@qq.com>
2024-09-10 22:58:41 +0800

    init commit

---
+++ src/main/java/cn/fo9c/educationaltourism/EducationalTourismApplication.java
@@ -0,0 +1,13 @@
+package cn.fo9c.educationaltourism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalTourismApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationalTourismApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/fo9c/educationaltourism/EducationalTourismApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.fo9c.educationaltourism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalTourismApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
