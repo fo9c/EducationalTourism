743ef965adb3e81fd7bcb013d971d21ebddbfec0
fo9c <1974555620@qq.com>
2024-09-11 17:03:20 +0800

    添加启动项

--- src/main/java/cn/fo9c/educationaltourism/EducationalTourismApplication.java
+++ src/main/java/cn/fo9c/educationaltourism/EducationalTourismApplication.java
@@ -1,13 +1,36 @@
 package cn.fo9c.educationaltourism;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class EducationalTourismApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EducationalTourismApplication.class, args);
+        // 新启动项追加路径显示
+
+        // URL的环境变量
+        ConfigurableApplicationContext application = SpringApplication.run(EducationalTourismApplication.class, args);
+        Environment env = application.getEnvironment();
+        String port = env.getProperty("server.port");
+        String property = env.getProperty("server.servlet.context-path");
+        String path = property == null ? "" :  property;
+
+        // SpringBoot的环境变量
+        String springBootVersion = SpringBootVersion.getVersion();
+
+        // Mybatis的环境变量
+        String mybatisVersion = "3.0.3";
+
+        System.out.println(
+                "———————————————————————————————————————————————————————————————————————————————————————————————————————————\n\t" +
+                "         Access URLs:   http://localhost:" + port + path + "              SpringBoot   Version: " + springBootVersion + "\n\t" +
+                "         DataBase:      MySQL                              Mybatis-Plus Version: " + mybatisVersion + "\n" +
+                "———————————————————————————————————————————————————————————————————————————————————————————————————————————");
+
     }
 
 }
