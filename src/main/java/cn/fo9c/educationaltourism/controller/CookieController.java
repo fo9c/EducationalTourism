4dde35e910e426d6fa9cbf49550b0eeddb7b8c1f
fo9c <1974555620@qq.com>
2024-09-15 19:27:50 +0800

    [Update]1

--- src/main/java/cn/fo9c/educationaltourism/controller/CookieController.java
+++ src/main/java/cn/fo9c/educationaltourism/controller/CookieController.java
@@ -1,14 +1,12 @@
 package cn.fo9c.educationaltourism.controller;
 
 import cn.fo9c.educationaltourism.utils.CookieUtils;
+import cn.fo9c.educationaltourism.utils.Result;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,7 +16,7 @@ public class CookieController {
     public String getCookie(HttpServletRequest request, @CookieValue(value = "username",
             defaultValue = "Atta") String username ) {
 
-        return CookieUtils.getCookieValue(request, "myCookie");
+        return CookieUtils.getCookieValue(request, "myqCookie", false);
     }
 
     @GetMapping("/setCookie")
@@ -26,9 +24,14 @@ public class CookieController {
 
 
         // 创建一个 Cookie
-        Cookie cookie = new Cookie("myCookie", "cookieValue");
+        Cookie cookie = new Cookie("myCookie", "co45468e");
         cookie.setMaxAge(600); // 设置 Cookie 的过期时间为 1 小时
         response.addCookie(cookie);
         return "setCookie";
+    }
+
+    @GetMapping("/1")
+    public Result<String> getCookie1() {
+        return Result.success("getCookie1");
     }
 }
--- src/main/java/cn/fo9c/educationaltourism/utils/CookieUtils.java
+++ src/main/java/cn/fo9c/educationaltourism/utils/CookieUtils.java
@@ -3,9 +3,24 @@ package cn.fo9c.educationaltourism.utils;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 
-public final class CookieUtils {
-    public static String getCookieValue(HttpServletRequest request, String cookieName) {
-        String retValue = null;
+import java.net.URLDecoder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public final class CookieUtils{
+    // Cookie 编码的字符集（默认为 UTF-8）
+    private final static Charset cookieEncoding = StandardCharsets.UTF_8;
+
+    /**
+     * 获取 Cookie 的值
+     * @param request HTTP 请求值
+     * @param cookieName 需要查找的 Cookie 名称
+     * @param isDecoder 是否解码
+     *                  true: 解码
+     *                  false: 不解码
+     * @return Cookie 的值
+     */
+    public static String getCookieValue(HttpServletRequest request, String cookieName, boolean isDecoder) {
 
         // 获取请求中的所有 Cookie 并判断是否为空
         Cookie[] cookieList = request.getCookies();
@@ -14,16 +29,41 @@ public final class CookieUtils {
         }
 
         // 遍历 Cookie 数组
-
-        for (Cookie cookie : cookieList) {
-            // 判断当前 Cookie 的名字是否与传入的名字相同
-            if (cookie.getName().equals(cookieName)) {
-                // 返回当前 Cookie 的值
-                return cookie.getValue();
+        try {
+            for (Cookie cookie : cookieList) {
+                if (cookie.getName().equals(cookieName)) {
+                    if (isDecoder) {
+                        return URLDecoder.decode(cookie.getValue(), cookieEncoding);
+                    } else {
+                        return cookie.getValue();
+                    }
+                }
             }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-
-        return "getCookieValue";
+        return null;
     }
 
+
+    /**
+     * 设置 Cookie 的值
+     * @param request HTTP 请求值
+     * @param cookieName Cookie 名称
+     * @param cookieValue Cookie 的值
+     * @param cookieMaxAge Cookie 的最大持续时间（单位：秒）
+     * @param isDecoder 是否编码
+     *                  true: 编码
+     *                  false: 不编码
+     * @return Cookie 的值
+     */
+    public static void setCookieValue(HttpServletRequest request, String cookieName, String cookieValue, int cookieMaxAge, boolean isDecoder) {
+        try {
+            Cookie cookie = new Cookie(cookieName, isDecoder ? URLDecoder.decode(cookieValue, cookieEncoding) : cookieValue);
+            cookie.setMaxAge(cookieMaxAge);
+            request.setAttribute(cookieName, cookie);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
